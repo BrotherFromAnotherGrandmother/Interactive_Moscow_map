28d84ad27767d43328dc3f81a7ef4c713ff33451
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2023-10-23 16:36:08 +0300

    Add Place model

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -4,7 +4,9 @@ from django.db import models
 # Create your models here.
 class Place(models.Model):
     title = models.TextField()
-    # img = models.ImageField()
     description_short = models.TextField()
     description_long = models.TextField()
     coordinates = models.JSONField()
+
+    def __str__(self):
+        return self.title
