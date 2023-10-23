24c46ea97854c8e70f43ac3632f8229e9f8c8baf
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2023-10-23 15:22:47 +0300

    Создал модель Place

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,3 +1,10 @@
+from django import forms
 from django.db import models
 
 # Create your models here.
+class Place(models.Model):
+    title = models.TextField()
+    img = models.ImageField()
+    description_short = models.TextField()
+    description_long = models.TextField()
+    coordinates = models.JSONField()
