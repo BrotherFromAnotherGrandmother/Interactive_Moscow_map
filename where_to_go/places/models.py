8d7031d45cf439cceb8b3d7a7cad4c468167efbf
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2023-10-23 15:29:51 +0300

    Убрал img поле модели Накатил миграции

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Place(models.Model):
     title = models.TextField()
-    img = models.ImageField()
+    # img = models.ImageField()
     description_short = models.TextField()
     description_long = models.TextField()
     coordinates = models.JSONField()
