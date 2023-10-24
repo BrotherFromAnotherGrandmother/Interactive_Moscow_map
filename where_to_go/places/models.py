2b0e52fdc3376e56263091cddda25bcc313bc299
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2023-10-24 12:09:54 +0300

    Создал и прописал динамически images.html Создал и подключил urls.py приложения places для отображения картинок Создал views.py для отображения картинок. Пока с некликабельными ссылками

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.db import models
 
+
 # Create your models here.
 class Place(models.Model):
     title = models.TextField()
@@ -13,6 +14,9 @@ class Place(models.Model):
 
 
 class Image(models.Model):
+    number = models.IntegerField()
     image = models.ImageField(upload_to='images/', blank=True)
-    place = models.ForeignKey(Place, on_delete = models.CASCADE)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.number} {self.place.__str__()}'
--- where_to_go/places/views.py
+++ where_to_go/places/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def get_media(request):
+    template = 'images.html'
+    urls = [
+        "https://raw.githubusercontent.com/devmanorg/where-to-go-places/master/media/4f793576c79c1cbe68b73800ae06f06f.jpg",
+        "https://raw.githubusercontent.com/devmanorg/where-to-go-places/master/media/7a7631bab8af3e340993a6fb1ded3e73.jpg",
+        "https://raw.githubusercontent.com/devmanorg/where-to-go-places/master/media/a55cbc706d764c1764dfccf832d50541.jpg",
+        "https://raw.githubusercontent.com/devmanorg/where-to-go-places/master/media/65153b5c595345713f812d1329457b54.jpg",
+        "https://raw.githubusercontent.com/devmanorg/where-to-go-places/master/media/0a79676b3d5e3b394717b4bf2e610a57.jpg",
+        "https://raw.githubusercontent.com/devmanorg/where-to-go-places/master/media/1e27f507cb72e76b604adbe5e7b5f315.jpg",
+    ]
+
+    context = {
+        'urls_img': urls
+    }
+
+
+    return render(request, template, context)
